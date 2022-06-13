e7865e6ed31421e63e67c2e70735b9c815162785
SDen4 <asw325@yandex.ru>
2022-06-13 15:31:41 +0500

    styles refactoring

--- src/lib/Button/Button.tsx
+++ src/lib/Button/Button.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+
 import { ButtonType } from './types';
+
+import styles from './styles.module.css';
 
 const Button: React.FC<ButtonType> = ({
   type = 'button',
@@ -12,7 +15,7 @@ const Button: React.FC<ButtonType> = ({
     type={type}
     onClick={onclick}
     disabled={disabled}
-    className={addClass}
+    className={`${styles.button} ${addClass}`}
   >
     {title}
   </button>
