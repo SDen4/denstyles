70aa532cfabc9a7edae34de3e004186e984219bc
SDen4 <asw325@yandex.ru>
2022-09-27 18:42:13 +0500

    refactor: update Loader

--- src/lib/Loader/Loader.tsx
+++ src/lib/Loader/Loader.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 import Flex from '../Flex/Flex';
 
+import { LoaderType } from './types';
+
 import styles from './styles.module.css';
 
-const Loader = (color?: 'rgb(10, 160, 10)') => {
+/**
+ *Loader component
+ *
+ * @param {*} { color }
+ * @return {*}
+ */
+
+const Loader: React.FC<LoaderType> = ({ color }) => {
   return (
     <Flex className={styles.loaderWrapper}>
       <div className={styles.ldsDefault}>
         {new Array(12).map((el) => (
-          <div style={{ background: color }} key={el} />
+          <div style={{ background: color || 'rgb(10, 160, 10)' }} key={el} />
         ))}
         <div />
       </div>
